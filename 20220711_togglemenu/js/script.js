2fd4e13f61f72f01f31cfd4bcd42a32d9506df06
chaejjji <chaeji3411@naver.com>
2022-07-11 17:27:11 +0900

    ..

--- 20220711_togglemenu/js/script.js
+++ 20220711_togglemenu/js/script.js
@@ -3,9 +3,11 @@ let view = true;
 $(".toggleMenu").click(function(){
     if( view == true){
         $(".topMenu").addClass("active");
+        $(".toggleMenu").addClass("click");
         view = false;
     }else{
         $(".topMenu").removeClass("active");
+        $(".toggleMenu").removeClass("click");
         view = true;
     }
 })
